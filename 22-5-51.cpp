af458bf1c10fe8df131a6af61997fca839a9972e
UraiMizuki <misawasachikaismywife@gmail.com>
2022-05-22 03:53:33 +0800

    new start

--- 22-5-51.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio.h>
-int main() {
-	printf("You are my best honey");
-
-
-
-	return 0;
-}
--- ForTest/ForTest.cpp
+++ ForTest/ForTest.cpp
@@ -178,4 +178,8 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+
 }
+
+
+
