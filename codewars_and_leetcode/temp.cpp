4e5d3e7b46addb2621dbf8c21f5420fb69fb82c2
Isaac Newton <misawasachikaismywife@gmail.com>
2023-02-16 02:26:28 +0800

    nothing

---
+++ codewars_and_leetcode/temp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 10;
+    cout << n << endl;
+    cout << hex << n << endl;
+    cout << dec << n << endl;
+    cout << oct << n << endl;
+  
+    return 0;
+    
+}
---
+++ learning/C_C++/3_DelFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    const char * FilePath = "C:\\Users\\abc\\Desktop\\shiyan.txt";
+    if(remove(FilePath) == 0)
+        cout << "File deleted successfully" << endl;
+    else
+        cout << "Error deleting file" << endl;
+
+
+    return 0;
+
+}
