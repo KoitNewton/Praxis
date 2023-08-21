915dce66024fa17a78be0932d3b3bcc145f3c292
Isaac Newton <misawasachikaismywife@gmail.com>
2023-08-21 13:56:46 +0800

    Create SumOfPositiveFactors.cpp

---
+++ learning/C_C++/SumOfPositiveFactors.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int dividend = 0;
+    cin >> dividend;  // your dividend or the denominator
+    int half = dividend/2 +1;
+    cout << half<<endl;
+    for(int i=1; i<= half; i++){
+        
+        if((dividend%i)==0){
+            if((dividend/i)>i)
+                sum = dividend/i + i + sum;
+            else if((dividend/i)==i)
+                sum = i + sum;            
+        }
+
+    }
+
+    cout<<sum<<endl;
+
+    return 0;
+}
